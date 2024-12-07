9790e01f4eb9f9b66b1edde15e8e1b0930eb4679
Yeruva Sai Jahnavi <77843177+yeruvasaijahnavi@users.noreply.github.com>
2024-12-07 23:26:29 +0530

    Modify footer

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,57 +1,66 @@
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { Title } from '@angular/platform-browser';
-import { ActivatedRoute, NavigationEnd, Router, RouterOutlet } from '@angular/router';
-import { delay, filter, map, tap } from 'rxjs/operators';
+import { Component, DestroyRef, inject, OnInit } from "@angular/core";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
+import { Title } from "@angular/platform-browser";
+import {
+	ActivatedRoute,
+	NavigationEnd,
+	Router,
+	RouterOutlet,
+} from "@angular/router";
+import { delay, filter, map, tap } from "rxjs/operators";
 
-import { ColorModeService } from '@coreui/angular';
-import { IconSetService } from '@coreui/icons-angular';
-import { iconSubset } from './icons/icon-subset';
+import { ColorModeService } from "@coreui/angular";
+import { IconSetService } from "@coreui/icons-angular";
+import { iconSubset } from "./icons/icon-subset";
 
 @Component({
-    selector: 'app-root',
-    template: '<router-outlet />',
-    imports: [RouterOutlet]
+	selector: "app-root",
+	template: "<router-outlet />",
+	imports: [RouterOutlet],
 })
 export class AppComponent implements OnInit {
-  title = 'CoreUI Angular Admin Template';
+	title = "Warehouse Dashboard";
 
-  readonly #destroyRef: DestroyRef = inject(DestroyRef);
-  readonly #activatedRoute: ActivatedRoute = inject(ActivatedRoute);
-  readonly #router = inject(Router);
-  readonly #titleService = inject(Title);
+	readonly #destroyRef: DestroyRef = inject(DestroyRef);
+	readonly #activatedRoute: ActivatedRoute = inject(ActivatedRoute);
+	readonly #router = inject(Router);
+	readonly #titleService = inject(Title);
 
-  readonly #colorModeService = inject(ColorModeService);
-  readonly #iconSetService = inject(IconSetService);
+	readonly #colorModeService = inject(ColorModeService);
+	readonly #iconSetService = inject(IconSetService);
 
-  constructor() {
-    this.#titleService.setTitle(this.title);
-    // iconSet singleton
-    this.#iconSetService.icons = { ...iconSubset };
-    this.#colorModeService.localStorageItemName.set('coreui-free-angular-admin-template-theme-default');
-    this.#colorModeService.eventName.set('ColorSchemeChange');
-  }
+	constructor() {
+		this.#titleService.setTitle(this.title);
+		// iconSet singleton
+		this.#iconSetService.icons = { ...iconSubset };
+		this.#colorModeService.localStorageItemName.set(
+			"coreui-free-angular-admin-template-theme-default"
+		);
+		this.#colorModeService.eventName.set("ColorSchemeChange");
+	}
 
-  ngOnInit(): void {
+	ngOnInit(): void {
+		this.#router.events
+			.pipe(takeUntilDestroyed(this.#destroyRef))
+			.subscribe((evt) => {
+				if (!(evt instanceof NavigationEnd)) {
+					return;
+				}
+			});
 
-    this.#router.events.pipe(
-        takeUntilDestroyed(this.#destroyRef)
-      ).subscribe((evt) => {
-      if (!(evt instanceof NavigationEnd)) {
-        return;
-      }
-    });
-
-    this.#activatedRoute.queryParams
-      .pipe(
-        delay(1),
-        map(params => <string>params['theme']?.match(/^[A-Za-z0-9\s]+/)?.[0]),
-        filter(theme => ['dark', 'light', 'auto'].includes(theme)),
-        tap(theme => {
-          this.#colorModeService.colorMode.set(theme);
-        }),
-        takeUntilDestroyed(this.#destroyRef)
-      )
-      .subscribe();
-  }
+		this.#activatedRoute.queryParams
+			.pipe(
+				delay(1),
+				map(
+					(params) =>
+						<string>params["theme"]?.match(/^[A-Za-z0-9\s]+/)?.[0]
+				),
+				filter((theme) => ["dark", "light", "auto"].includes(theme)),
+				tap((theme) => {
+					this.#colorModeService.colorMode.set(theme);
+				}),
+				takeUntilDestroyed(this.#destroyRef)
+			)
+			.subscribe();
+	}
 }
