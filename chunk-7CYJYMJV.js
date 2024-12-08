67642fd8e7454e5dc7ba0b0169671917295c555a
Yeruva Sai Jahnavi <77843177+yeruvasaijahnavi@users.noreply.github.com>
2024-12-08 12:38:55 +0530

    Auto-generated commit

---
+++ chunk-7CYJYMJV.js
@@ -0,0 +1,1 @@
+import"./chunk-JKOY2XUY.js";var t=[{path:"",loadComponent:()=>import("./chunk-ZNJXYLSR.js").then(o=>o.DashboardComponent),data:{title:$localize`Dashboard`}}];export{t as routes};
---
+++ chunk-YITBNA3N.js
@@ -0,0 +1,1 @@
+import"./chunk-JKOY2XUY.js";var o=[{path:"",loadComponent:()=>import("./chunk-VQR7WOI7.js").then(t=>t.AuditLogsComponent),data:{title:"Logs"}}];export{o as routes};
