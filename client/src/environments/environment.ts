6f44b1ea7c172402acdb0f092901c5f344c16d58
Yeruva Sai Jahnavi <77843177+yeruvasaijahnavi@users.noreply.github.com>
2024-12-10 21:45:25 +0530

    Update environment to localhost

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,8 @@
+// export const environment = {
+// 	production: true,
+// 	apiUrl: "https://storewiz-server-ysj.up.railway.app",
+// };
 export const environment = {
-	production: true,
-	apiUrl: "https://storewiz-server-ysj.up.railway.app",
+	production: false,
+	apiUrl: "http://localhost:3000",
 };
