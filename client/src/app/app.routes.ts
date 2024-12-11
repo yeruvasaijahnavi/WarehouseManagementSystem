e36d358579ed705f3ca3b9df17c1192974c0193f
Yeruva Sai Jahnavi <77843177+yeruvasaijahnavi@users.noreply.github.com>
2024-12-11 13:31:58 +0530

    Remove all extra routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -51,53 +51,6 @@ export const routes: Routes = [
 				loadChildren: () =>
 					import("./views/audit-logs/routes").then((m) => m.routes),
 			},
-			{
-				path: "theme",
-				loadChildren: () =>
-					import("./views/theme/routes").then((m) => m.routes),
-			},
-			{
-				path: "base",
-				loadChildren: () =>
-					import("./views/base/routes").then((m) => m.routes),
-			},
-			{
-				path: "buttons",
-				loadChildren: () =>
-					import("./views/buttons/routes").then((m) => m.routes),
-			},
-			{
-				path: "forms",
-				loadChildren: () =>
-					import("./views/forms/routes").then((m) => m.routes),
-			},
-			{
-				path: "icons",
-				loadChildren: () =>
-					import("./views/icons/routes").then((m) => m.routes),
-			},
-			{
-				path: "notifications",
-				loadChildren: () =>
-					import("./views/notifications/routes").then(
-						(m) => m.routes
-					),
-			},
-			{
-				path: "widgets",
-				loadChildren: () =>
-					import("./views/widgets/routes").then((m) => m.routes),
-			},
-			{
-				path: "charts",
-				loadChildren: () =>
-					import("./views/charts/routes").then((m) => m.routes),
-			},
-			{
-				path: "pages",
-				loadChildren: () =>
-					import("./views/pages/routes").then((m) => m.routes),
-			},
 		],
 	},
 	{
